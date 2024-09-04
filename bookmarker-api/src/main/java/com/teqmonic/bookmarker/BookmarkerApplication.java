e0c7cbe6bfe7094a51a7eec1809e400fd4681a3e
sakthi <sakthiece08@gmail.com>
2024-09-04 15:40:23 -0400

    Code format

--- bookmarker-api/src/main/java/com/teqmonic/bookmarker/BookmarkerApplication.java
+++ bookmarker-api/src/main/java/com/teqmonic/bookmarker/BookmarkerApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookmarkerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookmarkerApplication.class, args);
 	}
-
 }
